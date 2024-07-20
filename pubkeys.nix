33880e8b06e79b048fa5d270854a00c47ae026bf
Dominik Rathmer <rathmerdominik@outlook.de>
2024-07-20 19:42:25 +0200

    feature(pubkeys): Introduce pubkeys

---
+++ pubkeys.nix
@@ -0,0 +1,8 @@
+{
+  users = {
+    "dominik@linpc" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA98imQ9xSPWkqghH5ZNgETJ0sBwi5xSQbPgKUxVPrwa";
+  };
+  hosts = {
+    xenon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKD0K5LAZp/hlVfs30yF0iNhEeRkHHrBm6RtGEI9m9lh";
+  };
+}
