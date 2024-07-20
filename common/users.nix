567d2cc4cbe679fe935e7140a84236646bfe517d
Dominik Rathmer <rathmerdominik@outlook.de>
2024-07-20 21:21:39 +0200

    fix(users): Change to mutable

--- common/users.nix
+++ common/users.nix
@@ -2,6 +2,7 @@
   age.secrets.user-dominik.file = ../secrets/user-dominik.age;
 
   users = {
+    mutableUsers = false;
     groups.dominik.gid = 1000;
 
     users = {
@@ -10,7 +11,7 @@
         uid = 1000;
         isNormalUser = true;
         # hashedPasswordFile = config.age.secrets.user-dominik.path;
-        initialPassword = "dominik";
+        password = "dominik";
         openssh.authorizedKeys.keys = builtins.attrValues (import ../pubkeys.nix).users;
         extraGroups = ["wheel"];
         linger = true;
