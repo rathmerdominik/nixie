4f6540ef018e0ff43ced6ba7dba27752025b2dd8
Dominik Rathmer <rathmerdominik@outlook.de>
2024-07-20 20:17:14 +0200

    fix(microbin): RFC42

--- servers/xenon/microbin.nix
+++ servers/xenon/microbin.nix
@@ -4,11 +4,11 @@
   services.microbin = {
     enable = true;
     passwordFile = config.age.secrets.microbin.path;
-    settings = ''
-      MICROBIN_PUBLIC_PATH="https://microbin.${config.networking.domain}"
-      MICROBIN_QR=true
-      MICROBIN_ETERNAL_PASTA=true
-      MICROBIN_MAX_FILE_SIZE_UNENCRYPTED_MB=20480
-    '';
+    settings = {
+      MICROBIN_PUBLIC_PATH = "https://microbin.${config.networking.domain}";
+      MICROBIN_QR = true;
+      MICROBIN_ETERNAL_PASTA = true;
+      MICROBIN_MAX_FILE_SIZE_UNENCRYPTED_MB = 20480;
+    };
   };
 }
