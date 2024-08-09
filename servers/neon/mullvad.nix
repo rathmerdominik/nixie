8f32fe745245f0a741c29d66e93a50e3505f1f75
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-09 21:28:00 +0200

    service

--- servers/neon/mullvad.nix
+++ servers/neon/mullvad.nix
@@ -7,17 +7,28 @@
 
   services.mullvad-vpn.enable = true;
 
-  systemd.services.mullvad-install = {
-    script = let
-      mullvad = lib.getExe' config.services.mullvad-vpn.package "mullvad";
-    in ''
-      ${mullvad} account set "$(cat ${config.age.secrets.mullvad.path})"
-      ${mullvad} relay set location nl
-      ${mullvad} connect
-      ${mullvad} auto-connect set on
-      ${mullvad} lan set allow
-    '';
-    after = ["mullvad-daemon.service"];
-    requires = ["mullvad-daemon.service"];
-  };
+  # systemd.services.mullvad-install = {
+  #   script = let
+  #     mullvad = lib.getExe' config.services.mullvad-vpn.package "mullvad";
+  #   in ''
+  #     ${mullvad} account set "$(cat ${config.age.secrets.mullvad.path})"
+  #     ${mullvad} relay set location nl
+  #     ${mullvad} connect
+  #     ${mullvad} auto-connect set on
+  #     ${mullvad} lan set allow
+  #   '';
+  #   wantedBy = ["mullvad-daemon.service"];
+  #   after = ["mullvad-daemon.service"];
+  #   requires = ["mullvad-daemon.service"];
+  # };
+
+  systemd.services.mullvad-daemon.postStart = let
+    mullvad = lib.getExe' config.services.mullvad-vpn.package "mullvad";
+  in ''
+    ${mullvad} account set "$(cat ${config.age.secrets.mullvad.path})"
+    ${mullvad} relay set location nl
+    ${mullvad} connect
+    ${mullvad} auto-connect set on
+    ${mullvad} lan set allow
+  '';
 }
