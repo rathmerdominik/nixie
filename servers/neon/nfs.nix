f3f144c7c596bd1cf975b280425825d01d7cd26e
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-10 17:32:58 +0200

    stuff

--- servers/neon/nfs.nix
+++ servers/neon/nfs.nix
@@ -4,11 +4,11 @@
     exports = ''
       /srv/torrents     192.168.178.0(no_subtree_check,rw,all_squash,anonuid=0,anongid=0)
     '';
-    extraNfsdConfig = ''
-      UDP=off
-      vers2=off
-      vers3=off
-    '';
+    settings.nfsd = {
+      UDP = false;
+      vers2 = false;
+      vers3 = false;
+    };
   };
 
   systemd.tmpfiles.settings."10-nfs"."/srv/torrents".d = {
