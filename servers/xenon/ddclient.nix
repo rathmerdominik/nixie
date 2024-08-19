e161ab91cb752049af5b587f01a6b5c75568ba69
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-19 21:25:50 +0200

    nixpkgs update

--- servers/xenon/ddclient.nix
+++ servers/xenon/ddclient.nix
@@ -2,10 +2,12 @@
   age.secrets.cloudflare-token.file = ../../secrets/cloudflare-token.age;
   services.ddclient = {
     enable = true;
+    usev6 = ;
     protocol = "cloudflare";
     zone = "hammerclock.net";
     username = "token";
     passwordFile = config.age.secrets.cloudflare-token.path;
     domains = ["@"];
+    extraConfig = "ipv6=yes\nipv4=no";
   };
 }
