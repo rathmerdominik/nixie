6cc3d00322aa1fde2309abe4bcb0d7d19fc86026
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-08-20 10:34:02 +0200

    pubkey

--- pubkeys.nix
+++ pubkeys.nix
@@ -1,6 +1,7 @@
 {
   users = {
     "dominik@linpc" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA98imQ9xSPWkqghH5ZNgETJ0sBwi5xSQbPgKUxVPrwa";
+    "dominik@AGAN-NB059" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB67nwX6gfNFhzAQYASfxBMYhws7N9lK3j0eGlbSvDxv";
   };
   hosts = {
     xenon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKD0K5LAZp/hlVfs30yF0iNhEeRkHHrBm6RtGEI9m9lh";
