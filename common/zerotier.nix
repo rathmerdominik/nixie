15214a743c0dfb7a82cf5d259d5adb17d847a243
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-21 17:51:06 +0200

    zerotier

---
+++ common/zerotier.nix
@@ -0,0 +1,12 @@
+{
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = ["a84ac5c10acf5761"];
+  };
+
+  networking.hosts = {
+    "10.147.18.12" = ["neon"];
+    "10.147.18.11" = ["krypton"];
+    "10.147.18.10" = ["xenon"];
+  };
+}
--- servers/neon/zerotier.nix
+++
@@ -1,6 +0,0 @@
-{
-  services.zerotierone = {
-    enable = true;
-    joinNetworks = ["a84ac5c10acf5761"];
-  };
-}
