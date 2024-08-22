f9c6f3f6508e3503b7c739af46baa69c24b398f8
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-08-22 15:21:39 +0200

    argon finish up

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -12,4 +12,6 @@ with import ../pubkeys.nix; {
   "pterodactyl-env.age".publicKeys = (builtins.attrValues users) ++ [hosts.krypton];
   "vaultwarden-env.age".publicKeys = (builtins.attrValues users) ++ [hosts.krypton];
   "zerotier-api.age".publicKeys = (builtins.attrValues users) ++ [hosts.krypton];
+
+  "authentik-env.age".publicKeys = (builtins.attrValues users) ++ [hosts.argon];
 }
--- servers/argon/authentik.nix
+++ servers/argon/authentik.nix
@@ -1,5 +1,27 @@
-{
+{config, ...}: {
+  age.secrets.authentik-env.file = ../../secrets/authentik-env.age;
+
   services.authentik = {
     enable = true;
+    environmentFile = config.age.secrets.authentik-env.path;
+    settings = {
+      email = {
+        host = "hammerclock.net";
+        port = 587;
+        username = "auth@hammerclock.net";
+        use_tls = true;
+        use_ssl = false;
+        from = "auth@hammerclock.net";
+      };
+      disable_startup_analytics = true;
+      avatars = "initials,gravatar";
+    };
+    nginx = {
+      enable = true;
+      enableACME = true;
+      host = "auth.hammerclock.net";
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [80 443];
 }
