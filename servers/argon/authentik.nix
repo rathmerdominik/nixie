c1924a51df4e3adfbe0915c9317f8517b2211c2a
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-25 04:01:36 +0200

    panel

--- servers/argon/authentik.nix
+++ servers/argon/authentik.nix
@@ -1,29 +1,2 @@
 {
-  pkgs,
-  config,
-  ...
-}: {
-  services.nginx = {
-    enable = true;
-
-    package = pkgs.nginxQuic;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    commonHttpConfig = ''
-      error_log stderr;
-      access_log /var/log/nginx/access.log;
-    '';
-
-    virtualHosts."~.*" = {
-      default = true;
-      rejectSSL = true;
-
-      globalRedirect = config.networking.domain;
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [80 443];
 }
