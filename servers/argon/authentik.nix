ae8a2d7129d95b67b30ad8c237241580523f1caf
Dominik Rathmer <rathmerdominik@outlook.de>
2024-08-26 00:47:23 +0200

    authentik

--- servers/argon/authentik.nix
+++ servers/argon/authentik.nix
@@ -2,15 +2,4 @@
   inherit (config.networking) domain;
 in {
   age.secrets.mail-hammerclock.file = ../../secrets/mail-hammerclock.age;
-
-  services.authentik = {
-    enable = true;
-    environmentFile = config.age.secrets.mail-hammerclock.file.path;
-
-    nginx = {
-      enable = true;
-      enableACME = true;
-      host = "auth.${domain}";
-    };
-  };
 }
