f78b08607c629266b8bd5a4b93a8d0877cdb15e1
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-08-26 15:11:14 +0200

    authentik

--- servers/argon/authentik.nix
+++ servers/argon/authentik.nix
@@ -6,5 +6,22 @@ in {
   services.authentik = {
     enable = true;
     environmentFile = config.age.secrets.mail-hammerclock.path;
+    settings = {
+      email = {
+        host = "mail.${domain}";
+        port = 465;
+        username = "authentik@${domain}";
+        use_tls = true;
+        use_ssl = true;
+        from = "authentik@${domain}";
+      };
+      disable_startup_analytics = true;
+      avatars = "attributes.avatar,gravatar,initials";
+    };
+    nginx = {
+      enable = true;
+      enableACME = true;
+      host = "auth.${domain}";
+    };
   };
 }
