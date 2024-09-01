4b4399434ea0e3fc6c20ec6f38423d203b94e131
Dominik Rathmer <rathmerdominik@outlook.de>
2024-09-01 23:05:48 +0200

    immich

--- servers/xenon/default.nix
+++ servers/xenon/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   imports =
     (map (n: ./${n}) (builtins.filter (name: name != "default.nix") (builtins.attrNames (builtins.readDir ./.))))
     ++ [
@@ -6,8 +10,17 @@
       inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
       inputs.hardware.nixosModules.common-pc-ssd
     ];
-
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages;
+      if beta
+      then [nvidia_x11_beta]
+      else [nvidia_x11];
+
+    kernelParams = ["nvidia-drm.fbdev=1"];
+    initrd.kernelModules = ["nvidia"];
+  };
 
   system.stateVersion = "24.11";
 
@@ -18,5 +31,24 @@
   networking = {
     firewall.enable = false;
     domain = "hammerclock.net";
+  };
+
+  inputs.hardware.nvidia-container-toolkit.enable = true;
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+
+      open = false;
+
+      package = with config.boot.kernelPackages;
+        if beta
+        then nvidiaPackages.beta
+        else nvidiaPackages.latest;
+    };
   };
 }
