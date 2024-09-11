9f778c925c7c5b4d6544ad2f0c63dd5b9b1c6665
Dominik Rathmer <rathmerdominik@outlook.de>
2024-09-11 20:06:22 +0200

    nginx update

--- servers/argon/nginx.nix
+++ servers/argon/nginx.nix
@@ -88,6 +88,7 @@ in {
           extraConfig = ''
             proxy_buffering off;
             proxy_request_buffering off;
+            client_max_body_size 50000M;
           '';
         };
       };
