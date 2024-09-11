eec2dc2e6bc2f177e03a424237ff079b7cfcad80
Dominik Rathmer <rathmerdominik@outlook.de>
2024-09-11 18:13:37 +0200

    nfs

--- servers/xenon/flaresolverr.nix
+++ servers/xenon/flaresolverr.nix
@@ -1,5 +1,11 @@
 {
-  services.flaresolverr = {
-    enable = true;
+  virtualisation.oci-containers.containers.flaresolverr = {
+    image = "ghcr.io/flaresolverr/flaresolverr:latest";
+    ports = ["8191:8191"];
+    environment = {
+      "TZ" = "Europe/Berlin";
+      "HOST" = "10.147.18.10";
+      "PORT" = "8191";
+    };
   };
 }
