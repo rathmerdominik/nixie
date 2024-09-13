0d5f1931742bc72c1ff249672dc6d182b40182fe
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-09-13 17:30:34 +0200

    paperless

--- servers/xenon/paperless-ngx.nix
+++ servers/xenon/paperless-ngx.nix
@@ -38,9 +38,8 @@ in {
         continue_on_soft_render_error = true;
       };
 
-      PAPERLESS_TIKA_ENABLED = true;
-      PAPERLESS_TIKA_ENDPOINT = "http://localhost:9998";
-      PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://localhost:3000";
+      # PAPERLESS_TIKA_ENABLED = true;
+      # PAPERLESS_TIKA_ENDPOINT = "http://localhost:9998";
 
       PAPERLESS_CONSUMER_ENABLE_BARCODES = true;
       PAPERLESS_CONSUMER_ENABLE_ASN_BARCODE = true;
@@ -93,13 +92,14 @@ in {
     };
   };
 
-  services.tika = {
-    enable = true;
-    enableOcr = true;
-    listenAddress = "0.0.0.0";
-  };
+  # services.tika = {
+  #   enable = true;
+  #   enableOcr = true;
+  #   listenAddress = "0.0.0.0";
+  # };
 
-  services.gotenberg = {
-    enable = true;
-  };
+  # # This here core dumps...
+  # services.gotenberg = {
+  #   enable = true;
+  # };
 }
