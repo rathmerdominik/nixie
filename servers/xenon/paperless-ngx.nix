af368d444954373424717a53a4e8b54ee274840b
Dominik Rathmer <rathmerdominik@outlook.de>
2024-09-14 22:21:24 +0200

    nginx update

--- servers/xenon/paperless-ngx.nix
+++ servers/xenon/paperless-ngx.nix
@@ -65,8 +65,6 @@ in {
 
       PAPERLESS_APPS = "allauth.socialaccount.providers.openid_connect";
       PAPERLESS_SOCIAL_AUTO_SIGNUP = true;
-      PAPERLESS_REDIRECT_LOGIN_TO_SSO = true;
-      PAPERLESS_DISABLE_REGULAR_LOGIN = true;
       PAPERLESS_ACCOUNT_SESSION_REMEMBER = true;
     };
   };
