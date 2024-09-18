b57a8d4170503465743e113e53ad28ea83aad516
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-09-18 11:57:26 +0200

    romm

--- servers/argon/nginx.nix
+++ servers/argon/nginx.nix
@@ -24,7 +24,7 @@ in {
   };
 
   services.nginx = {
-    logErr = "/var/log/nginx/error.log debug";
+    logError = "/var/log/nginx/error.log debug";
     virtualHosts = {
       "~.*" = {
         default = true;
