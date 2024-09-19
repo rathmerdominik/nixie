b94b661c13e01cae21030c16730b7e1b9b0b398a
Dominik Rathmer <rathmerdominik@outlook.de>
2024-09-19 14:42:51 +0200

    fix stupid error

--- servers/argon/nginx.nix
+++ servers/argon/nginx.nix
@@ -51,7 +51,7 @@ in {
           extraConfig = ''
             proxy_buffering off;
             proxy_request_buffering off;
-            sclient_max_body_size 1024M;
+            client_max_body_size 1024M;
           '';
         };
       };
