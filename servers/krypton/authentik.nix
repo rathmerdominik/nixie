976c66ab74001594fcb41322b92a64b73069439d
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2024-11-14 12:56:23 +0100

    reintroduce authentik

--- servers/krypton/authentik.nix
+++ servers/krypton/authentik.nix
@@ -1,24 +1,22 @@
-{}
-# {config, ...}: let
-#   inherit (config.networking) domain;
-# in {
-#   age.secrets.authentik.file = ../../secrets/authentik.age;
-#   services.authentik = {
-#     enable = true;
-#     environmentFile = config.age.secrets.authentik.path;
-#     settings = {
-#       email = {
-#         host = "mail.${domain}";
-#         port = 465;
-#         username = "auth@${domain}";
-#         use_tls = true;
-#         use_ssl = true;
-#         from = "auth@${domain}";
-#       };
-#       disable_startup_analytics = true;
-#       avatars = "gravatar,initials";
-#     };
-#   };
-#   networking.firewall.allowedTCPPorts = [9070];
-# }
-
+{config, ...}: let
+  inherit (config.networking) domain;
+in {
+  age.secrets.authentik.file = ../../secrets/authentik.age;
+  services.authentik = {
+    enable = true;
+    environmentFile = config.age.secrets.authentik.path;
+    settings = {
+      email = {
+        host = "mail.${domain}";
+        port = 465;
+        username = "auth@${domain}";
+        use_tls = true;
+        use_ssl = true;
+        from = "auth@${domain}";
+      };
+      disable_startup_analytics = true;
+      avatars = "gravatar,initials";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [9070];
+}
