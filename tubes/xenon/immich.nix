d2929240a2028ddea0d9ea17b91a409abddf9f25
Dominik Rathmer <dominik.rathmer@asgoodasnew.com>
2025-04-01 10:13:33 +0200

    update

--- tubes/xenon/immich.nix
+++ tubes/xenon/immich.nix
@@ -6,7 +6,7 @@
   ...
 }: let
   immich-path = "/var/lib/immich";
-  immich-version = "v1.129.0";
+  immich-version = "v1.131.1";
 in {
   # TODO Wohoo they made a module. Will have to transition at some point. Will be painful because of postgres.
   age.secrets.immich.file = ../../secrets/immich.age;
