b3d61964d6c8bcb59afedeb67f65435af537e45f
Dominik Rathmer <dominik@rathmer.me>
2025-08-24 00:51:12 +0200

    Fixed collation

--- tubes/krypton/immich.nix
+++ tubes/krypton/immich.nix
@@ -8,7 +8,7 @@
 in {
   age.secrets.immich.file = ../../secrets/immich.age;
   services.immich = {
-    enable = false;
+    enable = true;
     openFirewall = true;
     package = unstable.legacyPackages.x86_64-linux.immich;
     settings = {
