f6651f6f1e9c0627399be6fc28fe618e0893db19
Dominik Rathmer <dominik@rathmer.me>
2025-08-24 00:52:29 +0200

    Fixed collation

--- tubes/krypton/postgres.nix
+++ tubes/krypton/postgres.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}: {
   services.postgresql = {
     enable = true;
-    #ensureDatabases = ["paperless"];
-    #ensureUsers = [
-    #  {
-    #    name = "paperless";
-    #    ensureDBOwnership = true;
-    #  }
-    #];
+    ensureDatabases = ["paperless"];
+    ensureUsers = [
+      {
+        name = "paperless";
+        ensureDBOwnership = true;
+      }
+    ];
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
