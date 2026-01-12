9df32b0d189684c9baf07a97456c2e339dc48ca4
Dominik Rathmer <dominik@rathmer.me>
2026-01-12 22:44:22 +0100

    test

--- tubes/krypton/wings.nix
+++ tubes/krypton/wings.nix
@@ -2,7 +2,7 @@
   virtualisation.oci-containers.containers.wings = {
     image = "ghcr.io/pelican-dev/wings:latest";
     ports = [
-      "9595:8080"
+      "9595:443"
       "2022:2022"
     ];
     volumes = [
